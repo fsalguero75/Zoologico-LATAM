881db16bb38294d0a66b1a610c9c4cffc1e49a60
arialdyscv <arialdyscv@gmail.com>
2022-07-05 23:23:50 -0400

    prueba commit

--- src/main/java/Habitat/Lista_Animales.java
+++ src/main/java/Habitat/Lista_Animales.java
@@ -4,4 +4,10 @@ package Habitat;
 * Muestra la lista de animales
 * */
 public class Lista_Animales {
+
+    public static void mostrar_Lista(){
+
+        System.out.println("soy la lista :D");
+    }
+
 }
